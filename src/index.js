74d74fb89e23ae0b97890481a899becbd82e2be5
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-04-21 21:21:43 +0300

    done app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter basename="/goose_track">
-     <App />
-     </BrowserRouter>
+    <BrowserRouter basename="/goose_track">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Account.js
@@ -0,0 +1,9 @@
+const Account = () => {
+  return (
+    <>
+      <h1>Account</h1>
+    </>
+  );
+};
+
+export default Account;
