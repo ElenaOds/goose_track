0d862186d59ec6f857cceca356e52f8d1dc79bf1
Elena Pedko <elpg.ods@gmail.com>
2023-04-21 17:19:27 +0300

    commit

---
+++ src/components/Devices.js
@@ -0,0 +1,15 @@
+const sizes = {
+    mobile: '375px',
+    tablet: '768px',
+    laptop: '1440px',
+  };
+
+  export const device = {
+    mobileMin: `(min-width: ${sizes.mobile})`,
+    mobileMax: `(max-width: ${sizes.mobile})`,
+    tabletMin: `(min-width: ${sizes.tablet})`,
+    tabletMax: `(max-width: ${sizes.tablet})`,
+    laptopMin: `(min-width: ${sizes.laptop})`,
+    laptopMax: `(max-width: ${sizes.laptop})`,
+   
+  };
