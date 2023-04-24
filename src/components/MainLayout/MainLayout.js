c223e672466c4cbaf53f434143761466184aedce
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-24 16:11:24 +0300

    Update MainLayout.js

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -2,17 +2,34 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import {Header} from 'components/Header/Header';
 import SideBar from 'components/SideBar/SideBar';
+import styles  from './MainLayout.module.css';
+import { useState} from "react";
 
 const MainLayout = () => {
+  const [isActivPage,setisActivPage]=useState(true)
+  
+  const changePageFalse =()=>{ 
+    setisActivPage(false)
+  }
+  const changePageTrue =()=>{ 
+    setisActivPage(true)
+  }
+
+  const changePage=[
+    changePageFalse,
+    changePageTrue
+  ]   
+
   return (
-    <>
-      <Header />
-      <SideBar />
-      <Suspense>
-        <Outlet />
-      </Suspense>
-    </>
+    <div className={styles.flex}>
+      <SideBar  changePage={changePage}/>
+      <div>
+        <Header isActivPage={isActivPage}/>     
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </div>      
+    </div>
   );
 };
-
 export default MainLayout;
