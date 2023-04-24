94b051c191bfc9d04dbd8db9a619084d3ffd312c
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-24 16:11:45 +0300

    Update Calendar.js

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,12 +1,12 @@
 import CalendarTable from "../../components/CalendarTable/CalendarTable";
-
+import styles from './Calendar.module.css';
 
 const Calendar = () => {
   return (
-    <>
-      <h1>Calendar</h1>
-    <CalendarTable/>
-    </>
+    <div className={styles.container}>
+      {/* <h1>Calendar</h1> */}
+      <CalendarTable/>
+    </div>
   );
 };
 
