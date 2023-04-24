0dd17fbc9e48d83a0dae46a71fe9d42249d63c9f
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-24 16:11:40 +0300

    Update Account.js

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,8 +1,10 @@
+import styles from './Account.module.css';
+
 const Account = () => {
   return (
-    <>
+    <div className={styles.container}>
       <h1>Account</h1>
-    </>
+    </div>
   );
 };
 
