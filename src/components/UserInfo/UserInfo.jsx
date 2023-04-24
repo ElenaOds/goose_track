ea50493b55e6b992a3a9234adcd25a2915a0c800
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-24 16:11:33 +0300

    Update UserInfo.jsx

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,12 +1,16 @@
+// import { useSelector } from 'react-redux';
+// import {selectUser} from '../../redux/auth/auth.selectors';
 import styles  from './UserInfo.module.css';
 
-export const  UserInfo =()=> { 
+export const  UserInfo =()=> {
     // привязать к глобальному стейту переменные url, name
-    const name= 'Goose';     
+    // const user= useSelector(selectUser);     
+    // const {email}=user;
+    // console.log(email);
     const url=null; 
-
-
+    const name="Goose";
     const firstLetter = name[0].toLocaleUpperCase();
+    
     return (  
         <>
             <p className={styles.name}>{name}</p>            
