e17ccafe901073c7540f5de755e7071a38bab792
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-24 16:11:36 +0300

    Update UserNav.jsx

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -2,14 +2,16 @@ import { NavLink } from 'react-router-dom';
 import { ReactComponent as IconUser } from '../../icons/icon-user.svg';
 import { ReactComponent as IconCalendar } from '../../icons/icon-calendar.svg';
 import styles from './UserNav.module.css';
+import PropTypes from "prop-types";
 
-export const UserNav = () => {
+export const UserNav = ({changePage}) => {
   return (
     <>
       <h3 className={styles.heading}>User Panel</h3>
 
       <div className={styles.userNav}>
         <NavLink
+          onClick={changePage[1]}
           to="/account"
           className={({ isActive }) =>
             isActive ? styles.activeLink : styles.navLink
@@ -24,6 +26,7 @@ export const UserNav = () => {
         </NavLink>
 
         <NavLink
+          onClick={changePage[0]}
           to="/calendar"
           className={({ isActive }) =>
             isActive ? styles.activeLink : styles.navLink
@@ -40,3 +43,7 @@ export const UserNav = () => {
     </>
   );
 };
+
+UserNav.propTypes = {        
+  changePage: PropTypes.array  
+}
