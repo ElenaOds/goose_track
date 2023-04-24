9528e6d744006912fe71b3a4967cb3de2567b55c
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-04-25 00:44:57 +0300

    fix header/app

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,22 +1,25 @@
-// import { useSelector } from 'react-redux';
-// import {selectUser} from '../../redux/auth/auth.selectors';
-import styles  from './UserInfo.module.css';
+import { useSelector } from 'react-redux';
+import styles from './UserInfo.module.css';
 
-export const  UserInfo =()=> {
-    // привязать к глобальному стейту переменные url, name
-    // const user= useSelector(selectUser);     
-    // const {email}=user;
-    // console.log(email);
-    const url=null; 
-    const name="Goose";
-    const firstLetter = name[0].toLocaleUpperCase();
-    
-    return (  
-        <>
-            <p className={styles.name}>{name}</p>            
-            {url!==null 
-            ? <img src={url} alt='' className={styles.avatar} />
-            : <div className={styles.avatar}>{firstLetter}</div>}                      
-        </>           
-    );    
-}
+import { selectUser } from 'redux/auth/auth.selectors';
+
+export const UserInfo = () => {
+  const profile = useSelector(selectUser);
+
+  // const {email}=user;
+  // console.log(email);
+  const url = null;
+  const name = profile.name;
+  const firstLetter = name[0].toLocaleUpperCase();
+
+  return (
+    <>
+      <p className={styles.name}>{name}</p>
+      {url !== null ? (
+        <img src={url} alt="" className={styles.avatar} />
+      ) : (
+        <div className={styles.avatar}>{firstLetter}</div>
+      )}
+    </>
+  );
+};
