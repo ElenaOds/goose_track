0e902789f7c7d009f0274e96c263a96ae9f051f7
Dima Samoilik <103739636+dimadimaya@users.noreply.github.com>
2023-04-25 22:04:08 +0300

    Private and Public Routes
    It's work but not work Lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,35 +5,39 @@ import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
 import MainLayout from './MainLayout/MainLayout';
 import { ToastContainer } from 'react-toastify';
-import { RestrictedRoute } from './RestrictedRoute';
-import { setAuthHeader } from 'redux/auth/auth.operations';
-import { useSelector } from 'react-redux';
+import { refreshUser, setAuthHeader } from 'redux/auth/auth.operations';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectToken } from 'redux/auth/auth.selectors';
+import { PrivateRoute } from './AuthRouts/PrivateRoute';
+import { PublicRoute } from './AuthRouts/PublicRoute';
+import { useEffect } from 'react';
+import Home from 'pages/Home/Home';
 
 const App = () => {
   const token = useSelector(selectToken);
   setAuthHeader(token);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [token, dispatch]);
+
   return (
     <>
-      <div id="user-modal"></div>
       <Routes>
-        <Route
-          path="register"
-          element={
-            <RestrictedRoute redirectTo="/calendar" component={<Register />} />
-          }
-        />
-        <Route
-          path="login"
-          element={
-            <RestrictedRoute redirectTo="/calendar" component={<Login />} />
-          }
-        />
-        <Route path="/" element={<MainLayout />}>
-          <Route path="/account" element={<Account />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/calendar/:month/:day" element={<Calendar />} />
+        <Route path="" element={<PublicRoute />}>
+          <Route path="" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route path="" element={<PrivateRoute />}>
+          <Route path="" element={<MainLayout />}>
+            <Route path="/account" element={<Account />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/calendar/:month/:day" element={<Calendar />} />
+          </Route>
         </Route>
       </Routes>
       <ToastContainer />
@@ -42,3 +46,10 @@ const App = () => {
 };
 
 export default App;
+
+// const Home = lazy(() => import('../pages/Home'));
+// const Account = lazy(() => import('./pages/Account/Account'));
+// const Calendar = lazy(() => import('./pages/Calendar/Calendar'));
+// const Login = lazy(() => import('./pages/Login/Login'));
+// const Register = lazy(() => import('./pages/Register/Register'));
+// const MainLayout = lazy(() => import('./MainLayout/MainLayout'));
