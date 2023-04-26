385c8221f82ae8e6b329d8fd2090cd8c67709777
Elena Pedko <106108433+ElenaOds@users.noreply.github.com>
2023-04-26 22:04:20 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,43 @@
+import { Route, Routes } from 'react-router-dom';
 import Account from 'pages/Account/Account';
 import Calendar from 'pages/Calendar/Calendar';
 import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
 import MainLayout from './MainLayout/MainLayout';
-import Home from 'pages/Home/Home';
-import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import { RestrictedRoute } from './RestrictedRoute';
-import { setAuthHeader } from 'redux/auth/auth.operations';
-import { useSelector } from 'react-redux';
+import { refreshUser, setAuthHeader } from 'redux/auth/auth.operations';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectToken } from 'redux/auth/auth.selectors';
+import { PrivateRoute } from './AuthRouts/PrivateRoute';
+import { PublicRoute } from './AuthRouts/PublicRoute';
+import { useEffect } from 'react';
+import Home from 'pages/Home/Home';
 
 const App = () => {
   const token = useSelector(selectToken);
   setAuthHeader(token);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [token, dispatch]);
+
   return (
     <>
-      <div id="user-modal"></div>
       <Routes>
-        <Route path="home" element={<Home />} />
-        <Route
-          path="register"
-          element={
-            <RestrictedRoute redirectTo="/calendar" component={<Register />} />
-          }
-        />
-        <Route
-          path="login"
-          element={
-            <RestrictedRoute redirectTo="/calendar" component={<Login />} />
-          }
-        />
-        <Route path="/" element={<MainLayout />}>
-          <Route path="/account" element={<Account />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/calendar/:month/:day" element={<Calendar />} />
+        <Route path="" element={<PublicRoute />}>
+          <Route path="" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route path="" element={<PrivateRoute />}>
+          <Route path="" element={<MainLayout />}>
+            <Route path="/account" element={<Account />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/calendar/:month/:day" element={<Calendar />} />
+          </Route>
         </Route>
       </Routes>
       <ToastContainer />
