4cbe7cdfa1ff73671310116cc03ec2d92b6e2a66
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-26 17:49:18 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,30 @@
-import styles from './Header.module.css';
-
-import PropTypes from 'prop-types';
-
-// import { selectTask } from '';
-// import { useSelector } from 'react-redux';
-
 import { ThemeToggler } from '../ThemeToggler/ThemeToggler';
 import { UserInfo } from '../UserInfo/UserInfo';
-
-import { ReactComponent as Burger } from '../../icons/menu.svg';
+import styles from './Header.module.css';
+// import { ReactComponent as Burger } from '../../icons/menu.svg';
 import { ReactComponent as LogoHeader} from '../../icons/logoheader.svg';
+import PropTypes from 'prop-types';
+
+
+
 
 export const Header = ({isActivPage}) => {
-  // const task = useSelector(selectTask); 
-  //  индикатор наличия тасок на этот день . переделать когда будет готов глобальный стейт на таски
+// const openMenu = () => {
+  //   console.log('Кнопка будет открывать модальное окно с сайтбар')
+  // };
+
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const toggleSidebar = () => {
+  //   setIsOpen(!isOpen);
+  // };
+
+  // burger
+
   const task='';
 
   return (
-    <div className={styles.header}>
+    <div className={styles.header}>      
       <div className={styles.flex}>        
         {isActivPage === false 
         ?( task===null
@@ -33,9 +40,9 @@ export const Header = ({isActivPage}) => {
           </div>
           :<p>Calendar</p>) 
         : (<p className={styles.titel}>User Profiler</p>)}
-        <button className={styles.button} type="button">
+        {/* <button className={styles.button} type="button">
           <Burger className={styles.burger} />
-        </button>
+        </button> */}
       </div>
       <div className={styles.flex}>
         <ThemeToggler />
