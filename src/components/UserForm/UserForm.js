238fa61f50f1489d63116c5a4c52905496f721ac
Elena Pedko <elpg.ods@gmail.com>
2023-04-27 18:18:47 +0300

    commit

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/auth.selectors';
-import { useFormik } from 'formik';
 import css from './UserForm.module.css';
 
 const UserForm = () => {
