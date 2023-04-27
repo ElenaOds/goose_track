2a3e2ba466b89fa5ba8f09c217cfdb1df32318d8
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-04-27 18:02:03 +0300

    Add layout and basic styles to Account Page and User Form

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,58 +1,129 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/auth.selectors';
+import { useFormik } from 'formik';
 import css from './UserForm.module.css';
 
 const UserForm = () => {
+  const [formData, setFormData] = useState({
+    userPhoto: null,
+    name: '',
+    email: '',
+    phone: '',
+    birthday: '',
+    skype: '',
+  });
+
+  // const dispatch = useDispatch();
+  const { userPhoto, name, birthday, email, phone, skype } =
+    useSelector(selectUser);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formDataObj = new FormData();
+    Object.keys(formData).forEach(key => {
+      formDataObj.append(key, formData[key]);
+    });
+    // dispatch();
+  };
+
   return (
-    <form className={css.userform}>
+    <form className={css.form} onSubmit={handleSubmit}>
       <input
-        className={css.userform__userPhoto}
+        className={css.uploader}
         type="file"
         id="userPhoto"
         name="userPhoto"
+        value={formData.userPhoto}
+        onChange={handleChange}
       />
-      <label className={css.userform__userPhoto__label} htmlFor="userPhoto">
-        
-        {/* <img
-          className={css.userform__userPhoto_img}
-          src=""
-        /> */}
+      <label className={css.uploader__label} htmlFor="userPhoto">
+        {userPhoto ? (
+          <img className={css.userPhoto} src={userPhoto} alt="user avatar" />
+        ) : (
+          <h3 className={css.userletter}>{name[0]}</h3>
+        )}
       </label>
 
-      <label htmlFor="username">User Name</label>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        placeholder="Enter your username"
-      />
+      <label className={css.label} htmlFor="user">
+        <h4 id="user" name="user">
+          {formData.name.length > 0 ? formData.name : name}
+        </h4>
+        User
+      </label>
 
-      <label htmlFor="birthday">Birthday</label>
-      <input type="date" id="birthday" name="birthday" />
+      <div className={css.container}>
+        <label className={css.label} htmlFor="name">
+          <p>User Name</p>
+          <input
+            className={css.input}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+            value={formData.name.length > 0 ? formData.name : name}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Enter your email"
-      />
+        <label className={css.label} htmlFor="birthday">
+          <p>Birthday</p>
+          <input
+            className={css.input}
+            type="date"
+            id="birthday"
+            name="birthday"
+            value={formData.birthday.length > 0 ? formData.birthday : birthday}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label htmlFor="phone">Phone</label>
-      <input
-        type="tel"
-        id="phone"
-        name="phone"
-        placeholder="Enter your phone number"
-      />
+        <label className={css.label} htmlFor="email">
+          <p>Email</p>
+          <input
+            className={css.input}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            value={formData.email.length > 0 ? formData.email : email}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label htmlFor="skype">Skype</label>
-      <input
-        type="text"
-        id="skype"
-        name="skype"
-        placeholder="Add a skype number"
-      />
+        <label className={css.label} htmlFor="phone">
+          <p>Phone</p>
+          <input
+            className={css.input}
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="Enter your phone number"
+            value={formData.phone.length > 0 ? formData.phone : phone}
+            onChange={handleChange}
+          />
+        </label>
 
-      <button type="submit" disabled>
+        <label className={css.label} htmlFor="skype">
+          <p>Skype</p>
+          <input
+            className={css.input}
+            type="text"
+            id="skype"
+            name="skype"
+            placeholder="Add a skype number"
+            value={formData.skype.length > 0 ? formData.skype : skype}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <button className={css.button} type="submit" disabled>
         Save changes
       </button>
     </form>
