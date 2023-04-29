fc60586a762f0cb50e832f1728ab0cf0d4d73cbe
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-04-29 18:40:31 +0300

    Change userform, fix bug of inputs value, add photo placeholder

---
+++ src/components/UserForm/UserFormSchema.js
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+
+const supportedFormats = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+const UserFormSchema = Yup.object().shape({
+  userPhoto: Yup.mixed()
+    .required('A file is required')
+    .test(
+      'fileFormat',
+      'Unsupported file format',
+      value => value && supportedFormats.includes(value.type)
+    ),
+  name: Yup.string().min(3, 'Too short').max(50, 'Too long'),
+  birthday: Yup.date(),
+  email: Yup.string().email('Invalid email'),
+  phone: Yup.string().matches(phoneRegExp, 'Phone number is not valid'),
+  skype: Yup.string().matches(phoneRegExp, 'Skype number is not valid'),
+});
+
+export default UserFormSchema;
