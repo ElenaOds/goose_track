35bcc75837f0de9e7fded97e8908205346c5e5ec
ed_bolma <ed.bolma21@gmail.com>
2023-04-29 17:26:25 +0200

    pagination as component ok 100% not connected with others

---
+++ src/components/PeriodPaginator/PeriodPaginator.jsx
@@ -0,0 +1,106 @@
+import { addDays, addMonths, format, subDays, subMonths } from 'date-fns';
+import { useState } from 'react';
+import { ReactComponent as IconArrowRight } from '../../icons/icon-arrow-right.svg';
+import { ReactComponent as IconArrowLeft } from '../../icons/icon-arrow-left.svg';
+
+import styles from './PeriodPaginator.module.css';
+
+export const PeriodPaginator = ({ isActivePage }) => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [activeBtn, setActiveBtn] = useState('');
+
+  const handleNextMonth = () => {
+    setActiveBtn('next');
+    setCurrentDate(addMonths(currentDate, 1));
+  };
+
+  const handlePrevMonth = () => {
+    setActiveBtn('prev');
+    setCurrentDate(subMonths(currentDate, 1));
+  };
+
+  const handleNextDay = event => {
+    setActiveBtn('next');
+    setCurrentDate(addDays(currentDate, 1));
+  };
+
+  const handlePrevDay = () => {
+    setActiveBtn('prev');
+    setCurrentDate(subDays(currentDate, 1));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {isActivePage ? (
+        <>
+          <div className={styles.name_container}>
+            <h1 className={styles.name}>{format(currentDate, 'MMMM Y')}</h1>
+          </div>
+          <div className={styles.buttons_container}>
+            <button
+              className={`${styles.button} ${styles.button_left}`}
+              type="button"
+              onClick={handlePrevMonth}
+            >
+              <IconArrowLeft
+                className={
+                  activeBtn === 'prev'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+            <button
+              className={`${styles.button} ${styles.button_right}`}
+              type="button"
+              id={'2'}
+              onClick={handleNextMonth}
+            >
+              <IconArrowRight
+                className={
+                  activeBtn === 'next'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.name_container}>
+            <h1 className={styles.name}>{format(currentDate, 'd MMMM Y')}</h1>
+          </div>
+          <div className={styles.buttons_container}>
+            <button
+              className={`${styles.button} ${styles.button_left}`}
+              type="button"
+              onClick={handlePrevDay}
+            >
+              <IconArrowLeft
+                className={
+                  activeBtn === 'prev'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+            <button
+              className={`${styles.button} ${styles.button_right}`}
+              type="button"
+              onClick={handleNextDay}
+            >
+              <IconArrowRight
+                className={
+                  activeBtn === 'next'
+                    ? `${styles.icon_active}`
+                    : `${styles.icon}`
+                }
+              />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
