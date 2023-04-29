f4d79fbbcdab94c5480372be01d8a99906d710a2
rostiq <rostiknatkha@gmail.com>
2023-04-29 21:21:16 +0300

    AddTaskBtn styles

--- src/components/AddTaskBtn/AddTaskBtn.jsx
+++ src/components/AddTaskBtn/AddTaskBtn.jsx
@@ -6,7 +6,10 @@ export const AddTaskBtn = () => {
   const [isOpen, setIsModalOpen] = useState(false);
   return (
     <>
-      <button className={styles.button} onClick={() => setIsModalOpen(true)}>AddTaskBtn</button>
+      <button className={styles.button} onClick={() => setIsModalOpen(true)}>
+        <div className={styles.plus}></div>
+        AddTaskBtn
+      </button>
       {isOpen && (<TaskModal onClose={() => setIsModalOpen(false)} data={"Hello"} />)}
     </>
   );
