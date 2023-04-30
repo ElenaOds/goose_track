dd2e796aef572b90c565af0a4d6f319e84249ddf
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-04-30 13:17:23 +0300

    Update UserFormSchema.js

--- src/components/UserForm/UserFormSchema.js
+++ src/components/UserForm/UserFormSchema.js
@@ -2,7 +2,6 @@ import * as Yup from 'yup';
 
 const phonePattern =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-const skypeIdPattern = '^[a-zA-Z][a-zA-Z0-9._]{5,31}$';
 
 const UserFormSchema = Yup.object().shape({
   userPhoto: Yup.string(),
@@ -12,10 +11,7 @@ const UserFormSchema = Yup.object().shape({
   phone: Yup.string()
     .min(8, 'Too short')
     .matches(phonePattern, 'Invalid phone number format'),
-  skype: Yup.string()
-    .min(6, 'Too short')
-    .max(32, 'Too long')
-    .matches(skypeIdPattern, 'Invalid skype id format'),
+  skype: Yup.string(),
 });
 
 export default UserFormSchema;
