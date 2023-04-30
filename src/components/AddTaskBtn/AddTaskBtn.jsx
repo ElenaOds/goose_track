0a22597b613a4f6254a63554d4d0194e51961358
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-30 08:29:05 +0300

    Update AddTaskBtn.jsx

--- src/components/AddTaskBtn/AddTaskBtn.jsx
+++ src/components/AddTaskBtn/AddTaskBtn.jsx
@@ -1,12 +1,16 @@
 import {useState} from "react";
 import {TaskModal } from "../TaskModal/TaskModal";
+import styles from './AddTaskBtn.module.css';
+import { ReactComponent as Plus } from '../../icons/plus.svg';
 
-export const  AddTaskBtn =()=> {  
+export const  AddTaskBtn =({date})=> {  
   const [isOpen,setIsModalOpen]=useState(false);  
   return (
     <div >       
-      <button onClick={() => setIsModalOpen(true)}>AddTaskBtn</button> 
-      {isOpen && (<TaskModal onClose={() => setIsModalOpen(false)} data={"Hello"}/>)}            
+      <button className={styles.btn} onClick={() => setIsModalOpen(true)}>
+      <Plus className={styles.logo} />
+        Add task</button> 
+      {isOpen && (<TaskModal onClose={() => setIsModalOpen(false)} date={date} />)}            
     </div>  
   );     
 };
