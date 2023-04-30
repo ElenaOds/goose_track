40f2db7c6f6e98f74d61549a2e39576f5374ccfe
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-04-30 11:23:43 +0300

    Update ColumnsTasksList.js

---
+++ src/components/ColumnsTasksList/ColumnsTasksList.js
@@ -0,0 +1,11 @@
+import styles from './ColumnTasksList.module.css';
+
+const ColumnTasksList = () => {
+return (
+    <div className={styles.wrapper}>
+        
+    </div>
+)
+}
+
+export default ColumnTasksList;
