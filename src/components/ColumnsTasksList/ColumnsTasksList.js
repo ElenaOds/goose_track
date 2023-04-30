edf2c55604456306789e382a9987f424acf16bc2
Rostik Natkha <106112250+rostiq@users.noreply.github.com>
2023-04-30 12:54:18 +0300

    TaskColumnCard meta

--- src/components/ColumnsTasksList/ColumnsTasksList.js
+++ src/components/ColumnsTasksList/ColumnsTasksList.js
@@ -4,12 +4,11 @@ import TaskColumnCard from 'components/TaskColumnCard/TaskColumnCard';
 import styles from './ColumnsTasksList.module.css';
 
 const ColumnsTasksList = ({ tasks }) => {
-    console.log(tasks, 'tasks');
   
     return (
       <div className={styles.wrapper}>
         {tasks.map((task) => (
-          <TaskColumnCard key={task.taskTitle} tasks={task} />
+          <TaskColumnCard key={task.taskTitle} task={task} />
         ))}
       </div>
     );
--- src/components/TaskColumnCard/TaskColumnCard.js
+++ src/components/TaskColumnCard/TaskColumnCard.js
@@ -1,13 +1,19 @@
 
 import styles from './TaskColumnCard.module.css';
 
-const TaskColumnCard = (task) => {
-    console.log(task,'tasks');
+const TaskColumnCard = (task ) => {
+    console.log(task, 'task');
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.card}>
-
+                <div className={styles.userpic}>P</div>
+                {/* <img className={styles.userpic} alt='userpic' src=''/> */}
+                <p className={styles.title}>Test</p>
+                <div className={styles.toolbar_container} >
+                    <p className={styles.date}>12:44</p>
+                    <div className={styles.sort}>Low</div>
+                </div>
             </div>
         </div>
     )
