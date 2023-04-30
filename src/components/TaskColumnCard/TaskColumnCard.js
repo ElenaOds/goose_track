49443d7ceda82baf67fa175b1a4d5cdc74ab9908
Rostik Natkha <106112250+rostiq@users.noreply.github.com>
2023-04-30 21:59:35 +0300

    task cards styles

--- src/components/TaskColumnCard/TaskColumnCard.js
+++ src/components/TaskColumnCard/TaskColumnCard.js
@@ -1,18 +1,22 @@
+// import TaskToolbar from "components/TaskToolbar/TaskToolbar";
 
 import styles from './TaskColumnCard.module.css';
 
-const TaskColumnCard = (task ) => {
+const TaskColumnCard = (task) => {
     console.log(task, 'task');
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.card}>
-                <div className={styles.userpic}>P</div>
-                {/* <img className={styles.userpic} alt='userpic' src=''/> */}
-                <p className={styles.title}>Test</p>
-                <div className={styles.toolbar_container} >
-                    <p className={styles.date}>12:44</p>
-                    <div className={styles.sort}>Low</div>
+                <p className={styles.title}>Testsfjhgljskdhglsdjsfvlsdnflkvjnsldfknvlkdfjsnvlkjdfsnlvkjnsdffhglsjkdhfg</p>
+                <div className={styles.meta} >
+                    <div className={styles.task_info}>
+                        <div className={styles.userpic}>P</div>
+                        {/* <img className={styles.userpic} alt='userpic' src=''/> */}
+                        <div className={styles.sort}>Low</div>
+                    </div>
+                    {/* <TaskToolbar/> */}
+                    <div>Toolbar</div>
                 </div>
             </div>
         </div>
