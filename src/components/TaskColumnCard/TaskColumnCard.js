9ff609021adf91b7fe243b946e49c72884599ca5
Elena Pedko <elpg.ods@gmail.com>
2023-04-30 23:03:25 +0300

    small fix

--- src/components/TaskColumnCard/TaskColumnCard.js
+++ src/components/TaskColumnCard/TaskColumnCard.js
@@ -1,7 +1,7 @@
 // import TaskToolbar from "components/TaskToolbar/TaskToolbar";
 
 import styles from './TaskColumnCard.module.css';
-import { UpDateTaskBtn } from 'components/upDateTaskBtn/upDateTaskBtn';
+// import { UpDateTaskBtn } from 'components/upDateTaskBtn/upDateTaskBtn';
 
 const TaskColumnCard = (task) => {
     console.log(task, 'task');
@@ -10,13 +10,13 @@ const TaskColumnCard = (task) => {
         <div className={styles.wrapper}>
             <div className={styles.card}>
 
-              /* <div className={styles.userpic}>P</div>
-                { <img className={styles.userpic} alt='userpic' src=''/> }
-                <p className={styles.title}>Test</p>
+             {/* <div className={styles.userpic}>P</div> */}
+                {/* { <img className={styles.userpic} alt='userpic' src=''/> } */}
+                {/* <p className={styles.title}>Test</p>
                 <div className={styles.toolbar_container} >
                     <p className={styles.date}>12:44</p>
                     <div className={styles.sort}>Low</div>
-                    <UpDateTaskBtn  id={'644d2aa23459f2c60583fc95'}/> */
+                    <UpDateTaskBtn  id={'644d2aa23459f2c60583fc95'}/>  </div> */}
                     
                <p className={styles.title}>Testsfjhgljskdhglsdjsfvlsdnflkvjnsldfknvlkdfjsnvlkjdfsnlvkjnsdffhglsjkdhfg</p>
                 <div className={styles.meta} >
