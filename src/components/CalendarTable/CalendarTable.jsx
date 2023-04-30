0f4eb3d1b123e5a68eb1301c0f21f77803e034b5
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-04-30 10:25:28 +0300

    done route from choosenMonth

--- src/components/CalendarTable/CalendarTable.jsx
+++ src/components/CalendarTable/CalendarTable.jsx
@@ -1,64 +1,78 @@
 import styles from './CalendarTable.module.css';
 import { v4 as uuidv4 } from 'uuid';
-import {AddTaskBtn} from 'components/AddTaskBtn/AddTaskBtn';
+import { AddTaskBtn } from 'components/AddTaskBtn/AddTaskBtn';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'redux/tasks/tasks.operations';
 import { selectTaskList } from 'redux/tasks/tasks.selectors';
-const { getDate, format, addMonths } = require("date-fns");
+import { useNavigate } from 'react-router-dom';
+const { getDate, format, addMonths } = require('date-fns');
 
-export default function CalendarTable({weeksList}) {
+export default function CalendarTable({ weeksList }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const currentDate = Date.now();
-  const from  = format(currentDate, "yyyy-MM-dd");
-  const to = format(addMonths(currentDate, 1), "yyyy-MM-dd")
+  const from = format(currentDate, 'yyyy-MM-dd');
+  const to = format(addMonths(currentDate, 1), 'yyyy-MM-dd');
 
   useEffect(() => {
     const data = {
       from,
-      to
-    }
+      to,
+    };
     dispatch(get(data));
   }, [dispatch, from, to]);
 
   const select = useSelector(selectTaskList);
-  
-  
 
-  const handeClick = () => {
-    
-  }
+  const handeClick = date => {
+    date = new Date(date);
+    const formattedDate = format(date, 'dMMMMyyyy');
+    const result = formattedDate.charAt(0) + formattedDate.slice(1);
+    console.log('result', result);
+    navigate(`/calendar/day/${result}`);
+  };
 
   return (
-    <div className = {styles.container}>
-    <div className={styles.datesInCalendar}>
-        {weeksList.map((week) => {
+    <div className={styles.container}>
+      <div className={styles.datesInCalendar}>
+        {weeksList.map(week => {
           return (
             <div key={week} className={styles.week}>
-              {week.map((day) => {
-                const numberDay = getDate(day)
-                
-                const task = select.find((task) => {
-                  const date = new Date(task.date); 
-                  const days = date.getUTCDate();  
+              {week.map(day => {
+                const numberDay = getDate(day);
+
+                const task = select.find(task => {
+                  const date = new Date(task.date);
+                  const days = date.getUTCDate();
                   return days === numberDay;
                 });
                 const priorityColors = {
                   low: '#72C2F8',
                   medium: '#F3B249',
-                  high: '#EA3D65'
+                  high: '#EA3D65',
                 };
-                let taskColor = priorityColors[task?.priority]
+                let taskColor = priorityColors[task?.priority];
                 const taskStyle = { backgroundColor: taskColor };
                 return (
-                  <div className={styles.oneDay} key = {uuidv4()} onClick = {handeClick}>  
-                    {day ? <div><p className={styles.datesInCalendar_date}>{numberDay}</p>
-                      <div className={styles.task} style = {taskStyle}>
-                        {task && <p className={styles.task_text}>{task.title}</p>}
+                  <div
+                    className={styles.oneDay}
+                    key={uuidv4()}
+                    onClick={() => handeClick(day)}
+                  >
+                    {day ? (
+                      <div>
+                        <p className={styles.datesInCalendar_date}>
+                          {numberDay}
+                        </p>
+                        <div className={styles.task} style={taskStyle}>
+                          {task && (
+                            <p className={styles.task_text}>{task.title}</p>
+                          )}
                         </div>
-                      </div> 
-                      : null} 
+                      </div>
+                    ) : null}
                   </div>
                 );
               })}
@@ -66,8 +80,7 @@ export default function CalendarTable({weeksList}) {
           );
         })}
       </div>
-      <AddTaskBtn/>
+      <AddTaskBtn />
     </div>
-   
   );
 }
--- src/components/ChosenDay/ChosenDay.js
+++ src/components/ChosenDay/ChosenDay.js
@@ -3,23 +3,20 @@
 // import { useNavigate } from 'react-router-dom';
 import DayCalendarHead from 'components/DayCalendarHead/DayCalendarHead';
 import styles from './ChosenDay.module.css';
-
 import { useState } from 'react';
 import TasksColumnsList from 'components/TasksColumnsList/TasksColumnsList';
+import { parse } from 'date-fns';
+import { useParams } from 'react-router-dom';
 
-const ChosenDay = ({ selectDay }) => {
-  // const currentDay = format(Date.now(), 'ddMMMMyyyy');
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   navigate(`/calendar/day/${currentDay}`);
-  // }, [navigate, currentDay]);
+const ChosenDay = () => {
+  const { currentDay } = useParams();
+  const formattedDate = parse(currentDay, 'dMMMMyyyy', new Date());
 
   const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const currentDate = selectDay ? new Date(selectDay) : new Date();
+  const selectDay = formattedDate;
 
-  const currentDayOfWeek = currentDate.getDay();
-  const [selectedDay, setSelectedDay] = useState(currentDate);
+  const currentDayOfWeek = selectDay.getDay();
+  const [selectedDay, setSelectedDay] = useState(selectDay);
   const days = [];
 
   const handleDayClick = date => {
@@ -28,8 +25,8 @@ const ChosenDay = ({ selectDay }) => {
   console.log(selectedDay);
 
   for (let i = 0; i < 7; i++) {
-    const date = new Date(currentDate);
-    date.setDate(currentDate.getDate() + i - currentDayOfWeek);
+    const date = new Date(selectDay);
+    date.setDate(selectDay.getDate() + i - currentDayOfWeek);
 
     const isDaySelected =
       selectedDay && selectedDay.getDate() === date.getDate();
