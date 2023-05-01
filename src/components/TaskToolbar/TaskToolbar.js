d142328adfd1606eb4414429496c88fe26fcca80
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-05-01 14:10:16 +0300

    Update TaskToolbar.js

--- src/components/TaskToolbar/TaskToolbar.js
+++ src/components/TaskToolbar/TaskToolbar.js
@@ -3,11 +3,11 @@ import { Delete } from './Delete/Delete';
 import { Move } from './Move/Move';
 import styles from './TaskToolbar.module.css';
 
-export const TaskToolbar = ({ date, id }) => {
+export const TaskToolbar = ({id }) => {
   return (
     <div className={styles.toolbar}>
       <Move />
-      <UpDateTaskBtn date={date} id={id} />
+      <UpDateTaskBtn id={id} />
       <Delete id={id} />
     </div>
   );
