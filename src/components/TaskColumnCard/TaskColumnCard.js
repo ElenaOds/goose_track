6c745785a2fec4fdf046a8792fdc61da06c50f3d
Rostik Natkha <712020:290bcbd0-2456-4861-91af-617adaf10b25@bots.bitbucket.org>
2023-05-01 23:49:21 +0300

    task-priority-marker

--- src/components/TaskColumnCard/TaskColumnCard.js
+++ src/components/TaskColumnCard/TaskColumnCard.js
@@ -5,7 +5,7 @@ import styles from './TaskColumnCard.module.css';
 
 const TaskColumnCard = ({ title, _id, priority, firstLetter }) => { 
 
-
+console.log(priority);
   return (
     <div className={styles.wrapper}>
       <div className={styles.card}>
@@ -13,11 +13,11 @@ const TaskColumnCard = ({ title, _id, priority, firstLetter }) => {
           {title}
         </p>
         <div className={styles.meta}>
-          <div className={styles.task_info}>
+          <div className={`${styles.task_info} ${priority === 'high' ? styles.highPriority : priority === 'medium' ? styles.mediumPriority : styles.lowPriority}`}>
             <div className={styles.userpic}>
             {firstLetter}
             </div>
-            <div className={styles.sort}>{priority}</div>
+            <div className={`${styles.priority_main} ${styles.priority}`}>{priority}</div>
           </div>
           <TaskToolbar id={_id} />
         </div>
