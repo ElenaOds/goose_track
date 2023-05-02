c266498e0061f7a61a58c45ae7ff52b8c858e898
Dima Samoilik <103739636+dimadimaya@users.noreply.github.com>
2023-05-02 20:57:43 +0300

    correct render tasks cards
    correct render tasks cards

--- src/components/TaskToolbar/Delete/Delete.jsx
+++ src/components/TaskToolbar/Delete/Delete.jsx
@@ -1,12 +1,21 @@
 import { useDispatch } from 'react-redux';
-import { deleteItem } from 'redux/tasks/tasks.operations';
+import { deleteItem, get } from 'redux/tasks/tasks.operations';
 import { ReactComponent as DeleteIcon } from '../../../icons/delete.svg';
 import styles from './Delete.module.css';
+const { format, addMonths } = require('date-fns');
 
 export const Delete = ({ id }) => {
+  const currentDate = Date.now();
+  const from = format(currentDate, 'yyyy-MM-dd');
+  const to = format(addMonths(currentDate, 1), 'yyyy-MM-dd');
+  const data = {
+    from,
+    to,
+  };
   const dispatch = useDispatch();
   const handleDelete = () => {
     dispatch(deleteItem(id));
+    dispatch(get(data));
   };
   return (
     <>
