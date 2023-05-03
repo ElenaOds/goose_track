23b02f8193130ab9824b9ab61eac1b4e2bcd1eb7
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-05-03 18:27:03 +0300

    Update TaskModal.jsx

--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -2,10 +2,10 @@ import { Modal } from '../Modal/Modal';
 import {TaskForm} from '../TaskForm/TaskForm';
 
 
-export const TaskModal =({date,onClose})=> {  
+export const TaskModal =({date,onClose,column})=> {  
   return (
     <Modal onClose={onClose}>      
-      <TaskForm date={date} onClose={onClose}/>                      
+      <TaskForm date={date} onClose={onClose} column={column}/>                      
     </Modal>
   );     
 };
