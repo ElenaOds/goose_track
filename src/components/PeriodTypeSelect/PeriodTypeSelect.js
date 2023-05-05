00ecc79bca3550ff1b35904884db9223f156e96a
ed_bolma <ed.bolma21@gmail.com>
2023-05-05 07:02:09 +0200

    day-month switch gets to today on Day section

--- src/components/PeriodTypeSelect/PeriodTypeSelect.js
+++ src/components/PeriodTypeSelect/PeriodTypeSelect.js
@@ -1,13 +1,18 @@
 import { NavLink } from 'react-router-dom';
-import { format } from 'date-fns';
+import { format, isSameMonth } from 'date-fns';
 import PropTypes from 'prop-types';
 import styles from './PeriodTypeSelect.module.css';
 import { useDate } from 'hooks/useDate';
 
 export const PeriodTypeSelect = ({ activePage }) => {
   const urlDate = useDate();
+  const today = new Date();
+
   const currentDate = format(urlDate, 'MMMMyyyy');
   const currentDay = format(urlDate, 'ddMMMMyyyy');
+  const formattedToday = format(today, 'ddMMMMyyyy');
+
+  const isToday = isSameMonth(urlDate, today);
 
   return (
     <div className={styles.periodTypeSelectMarkUp}>
@@ -22,7 +27,11 @@ export const PeriodTypeSelect = ({ activePage }) => {
         Month
       </NavLink>
       <NavLink
-        to={`/calendar/day/${currentDay}`}
+        to={
+          isToday
+            ? `/calendar/day/${formattedToday}`
+            : `/calendar/day/${currentDay}`
+        }
         className={
           activePage === 'day' ? styles.activeLink_day : styles.navLink_day
         }
